f5257575fd558ee91c673cc642cce02ce614e60a
mathiassommerw-pixel <mathiassommerw@gmail.com>
2026-05-04 23:16:02 +0200

    Tilføj service worker

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='solvejr-v1';
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){
+    return c.addAll(['/','manifest.json']);
+  }));
+});
+self.addEventListener('fetch',function(e){
+  e.respondWith(fetch(e.request).catch(function(){
+    return caches.match(e.request);
+  }));
+});
